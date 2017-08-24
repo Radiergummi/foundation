23f112b34f7dc908d67aca2817aa8e1ea84f94ae
Mo <m@9dev.de>
2017-08-24 13:36:25 +0200

    added coverage reporters

--- peridot.php
+++ peridot.php
@@ -2,6 +2,8 @@
 
 use Evenement\EventEmitterInterface;
 use Peridot\Console\Environment;
+use Peridot\Reporter\CodeCoverage\AbstractCodeCoverageReporter;
+use Peridot\Reporter\CodeCoverageReporters;
 
 error_reporting( - 1 );
 
@@ -13,6 +15,7 @@ error_reporting( - 1 );
  * @returns void
  */
 return function( EventEmitterInterface $emitter ) {
+    ( new CodeCoverageReporters( $emitter ) )->register();
 
     /**
      * set the default path
@@ -23,9 +26,14 @@ return function( EventEmitterInterface $emitter ) {
                     ->setDefault( 'specs' );
     } );
 
+    $emitter->on( 'code-coverage.start', function( AbstractCodeCoverageReporter $reporter ) {
+        $reporter->addDirectoryToWhitelist( __DIR__ . '/src' );
+        $reporter->addDirectoryToWhitelist( __DIR__ . '/app' );
+    } );
+
     $emitter->on( 'peridot.end', function() {
         if ( isset( $GLOBALS['__debug_called'] ) ) {
-            echo PHP_EOL . "Debugging method called on " . $GLOBALS['__debug_called']  . " This will exit the build with code 2 to prevent leftover debugging code in production. Review the file and line and make sure there's no db() or dd() call before you commit the code again." . PHP_EOL;
+            echo PHP_EOL . "Debugging method called on " . $GLOBALS['__debug_called'] . " This will exit the build with code 2 to prevent leftover debugging code in production. Review the file and line and make sure there's no db() or dd() call before you commit the code again." . PHP_EOL;
             exit( 2 );
         }
     } );
